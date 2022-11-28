9f3d6e69699460432fb921209bacef62fec5389f
Fishale <fishale2500@gmail.com>
2022-11-28 19:21:06 +0700

    Main Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         SQRService service = new SQRService();
-        System.out.println(service.calculate(5000, 8000));
 
     }
 }
